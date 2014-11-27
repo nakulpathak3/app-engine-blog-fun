e0370135043bfccd2137430eb7274f35d7bb465d
Nakul Pathak <nakulpathak3@hotmail.com>
2014-11-27 02:00:37 -0500

    Added maps functionality to ascii-chan. Uses HostIP and Google Maps static map API.

--- ascii-chan-2/asciichan.py
+++ ascii-chan-2/asciichan.py
@@ -2,6 +2,8 @@ import os
 import webapp2
 import jinja2
 import time
+import urllib2
+from xml.dom import minidom
 
 from google.appengine.ext import db
 
@@ -20,21 +22,54 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+IP_URL = "http://api.hostip.info/?ip="
+def get_coords(ip):
+    url = IP_URL + ip
+    content = None
+    try:
+        content = urllib2.urlopen(url).read()
+    except URLError:
+        return
+
+    if content:
+        d = minidom.parseString(content)
+        coords = d.getElementsByTagName("gml:coordinates")
+        if coords and coords[0].childNodes[0].nodeValue:
+            lon, lat = coords[0].childNodes[0].nodeValue.split(',')
+            return db.GeoPt(lat, lon)
+
+gmaps_url = "https://maps.googleapis.com/maps/api/staticmap?size=380x260&sensor=false&"
+
+def gmaps_img(points):
+        markers = '&'.join("markers=%s,%s" % (p.lat, p.lon) for p in points)
+        return gmaps_url + markers
+
 class Art(db.Model):
     title = db.StringProperty(required = True)
     art = db.TextProperty(required = True)
     created = db.DateTimeProperty(auto_now_add = True)
+    coords = db.GeoPtProperty()
 
 class MainPage(Handler):
 
     def render_front(self, title="", art="", error=""):
         arts = db.GqlQuery("SELECT * FROM Art "
                             "ORDER BY created DESC ")
+        arts = list(arts) #prevents running of  multiple queries. Local copy of datastore to access as much as you want available now/
+        points = []
+        for a in arts:
+            if a.coords:
+                points.append(a.coords)
+        img_url = None
+        if points:
+            img_url = gmaps_img(points)
+
         time.sleep(1)
-        self.render("front.html", title= title, art= art, error = error, arts = arts)
+        self.render("front.html", title= title, art= art, error = error, arts = arts, img_url = img_url)
+
 
     def get(self):
-        self.render_front("front.html")
+        self.render_front()
 
     def post(self):
         title = self.request.get("title")
@@ -42,6 +77,11 @@ class MainPage(Handler):
 
         if title and art:
             a = Art(title = title, art = art)
+            #lookup coordinates add to art
+            coords = get_coords(self.request.remote_addr)
+            if coords:
+                a.coords = coords
+
             a.put()
 
             self.redirect("/")
